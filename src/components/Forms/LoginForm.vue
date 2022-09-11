c9d902c768ea4300bdb86e7592bddd65efa64b53
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-09-11 16:28:48 -0500

    feature: added the loginform component to handle existing users

---
+++ src/components/Forms/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form>
+    <h2>{{ title }}</h2>
+    <p>{{ explanation }}</p>
+
+    <label>Email: </label>
+    <input placeholder="Email" required type="text" v-model="email" />
+
+    <br /><br />
+
+    <label>Password: </label>
+    <input placeholder="Password" required type="password" />
+
+    <br /><br />
+
+    <button @click.prevent="signInHandler">Sign in</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "LoginForm",
+  components: {},
+  props: ["title", "explanation"],
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const signInHandler = () => {
+      console.log("Success!");
+
+      email.value = "";
+      password.value = "";
+    };
+
+    return {
+      email,
+      password,
+      signInHandler,
+    };
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,24 @@
       title="Sign Up for Bookface"
       explanation="This is an explanation for Bookface"
     />
+    <br /><br /><br /><br />
+    <LoginForm
+      title="Sign In to Bookface"
+      explanation="Only for existing users"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SignUpForm from "../components/Forms/SignUpForm.vue";
+import LoginForm from "../components/Forms/LoginForm.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     SignUpForm,
+    LoginForm,
   },
 });
 </script>
